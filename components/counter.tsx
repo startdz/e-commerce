e1955f8b6bce78875a07c96a06598f00befd37ea
Fuad Jabbar Dzakwan <fuadzakwan26@gmail.com>
2024-07-28 10:42:34 +0700

    fixed

--- components/counter.tsx
+++ components/counter.tsx
@@ -7,7 +7,11 @@ export const Counter = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <Button radius="full" onPress={() => setCount(count + 1)} className="bg-primaries">
+    <Button
+      className="bg-primaries"
+      radius="full"
+      onPress={() => setCount(count + 1)}
+    >
       Count is {count}
     </Button>
   );
